cff8a9da7a529091d982cb9abdaf08410b67b965
HS H <3262398@naver.com>
2023-05-26 10:53:41 +0900

    0526

---
+++ 0526-firebase-task/src/database/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDf0CPQiEXSszqkyk6RZX1ps8_KobwQSlA",
+  authDomain: "fir-task-236d3.firebaseapp.com",
+  projectId: "fir-task-236d3",
+  storageBucket: "fir-task-236d3.appspot.com",
+  messagingSenderId: "823322685513",
+  appId: "1:823322685513:web:8175853ed3ac3233fd0f98"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
